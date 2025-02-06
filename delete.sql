170fc60a4fe7fd6950091ef053417d0745470fa8
Beatrice Ganță <beatrice.ganta98@e-uvt.ro>
2025-02-06 14:05:27 +0200

    Create delete.sql

---
+++ delete.sql
@@ -0,0 +1,17 @@
+DELETE FROM Angajati WHERE id = 10;
+DELETE FROM Angajati WHERE id = 9;
+DELETE FROM Angajati WHERE id = 8;
+
+DELETE FROM Servicii_clienti WHERE id = 10;
+DELETE FROM Servicii_clienti WHERE id = 9;
+DELETE FROM Servicii_clienti WHERE id = 8;
+
+DELETE FROM Activitati_Rezervari WHERE id = 10;
+DELETE FROM Activitati_Rezervari WHERE id = 9;
+DELETE FROM Activitati_Rezervari WHERE id = 8;
+
+DELETE FROM Plati WHERE id = 10;
+DELETE FROM Plati WHERE id = 9;
+DELETE FROM Plati WHERE id = 8;
+
+DELETE FROM Camere WHERE id = 10;
